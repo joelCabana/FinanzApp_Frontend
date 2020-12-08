33b0f2d0492f4787eb3b8a291416cf01003708a9
joelCabana <joelxcabana@gmail.com>
2020-12-08 11:00:10 -0300

    Feat: add create user

--- src/app/components/account/create/create.component.ts
+++ src/app/components/account/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  user:User;
+  created:boolean;
+
+  constructor(private userService:UserService) {
+    this.user = new User();
+    this.created = false;
+   }
 
   ngOnInit(): void {
+  }
+
+  createUser(){
+    this.userService.createUser(this.user).subscribe(
+      (result) =>{
+           this.reset()
+           this.created = true;
+      },
+      error=>{
+        console.log(error);
+        alert("ERROR");
+      }
+    );
+  }
+
+  reset(){
+    this.user = new User();
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -24,13 +24,22 @@ export class UserService {
 
 
    updateUser(user:User){
-    console.log("INGRESO AL SERVICE UPDATE");
     const httpOptions  = {
       headers: new HttpHeaders({
         'Content-Type':'application/json'
       })
     }; 
     var body = JSON.stringify(user);
-    return this._http.put(this.URL_API+"/"+ user._id,body,httpOptions);;
+    return this._http.put(this.URL_API+user._id,body,httpOptions);
+  }
+
+  createUser(user:User){
+    const httpOptions  = {
+      headers: new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    };
+    var body = JSON.stringify(user);
+    return this._http.post(this.URL_API,body,httpOptions);
   }
 }
